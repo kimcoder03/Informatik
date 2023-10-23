fecacc552ca04d5928373280821fc256bad724ec
kimcoder03 <seouljkim03@gmail.com>
2023-10-23 09:31:34 +0200

    master

--- 2023.10.20/04_Uebungen/04practice.c
+++ 2023.10.20/04_Uebungen/04practice.c
@@ -17,7 +17,7 @@ sich aus den aufsteigend sortierten Ziffern ergibt.
 Wiederhole, bis zwei mal in Folge die gleiche Zahl herauskommt.
 
 Beispiel:
-
+Sort the number into (bigger to smaller) - (smaller to bigger)
 792 ~> 972 - 279 = 693
 693 ~> 963 - 369 = 594
 594 ~> 954 - 459 = 495
@@ -32,8 +32,107 @@ Beispiele: 'kaprekar_count_steps(792) == 3', 'kaprekar_count_steps(693) == 2',
 Hinweis: Sollte die Eingabe 'x' kleiner als 100 sein (also weniger als 3 Dezimalstellen besitzen), sollte sie trotzdem
 dreistellig behandelt werden, indem Nullen vorangesetzt werden. D.H. 64 ~> 640 - 046 = 594.
 */
+
+int Firstdigit(int x)
+{
+    return x / 100;
+}
+
+int Seconddigit(int y)
+{
+    return (y / 10) % 10;
+}
+
+int Thirddigit(int z)
+{
+    return z % 10;
+}
+
+int sortDesc(int x, int y, int z) {
+    int temp;
+    if (x < y) 
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    if (x < z) 
+    {
+        temp = x;
+        x = z;
+        z = temp;
+    }
+
+    if (y < z) 
+    {
+        temp = y;
+        y = z;
+        z = temp;
+    }
+
+    return x * 100 + y * 10 + z;
+}
+
+int sortAsce(int x, int y, int z) {
+    int temp;
+    if (x > y) 
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    if (x > z) 
+    {
+        temp = x;
+        x = z;
+        z = temp;
+    }
+
+    if (y > z) 
+    {
+        temp = y;
+        y = z;
+        z = temp;
+    }
+
+    return x * 100 + y * 10 + z;
+}
+
+int getsinglestep(int x) {
+    int a = Firstdigit(x);
+    int b = Seconddigit(x);
+    int c = Thirddigit(x);
+
+    int d = sortDesc(a, b, c);
+    int e = sortAsce(a, b, c);
+    return d - e;
+}
+
+int expandnumber(int x)
+{
+    while (x < 100)
+    {
+        x *= 10;
+    }
+    return x;
+}
+
+
 int kaprekar_count_steps(int x) {
-    return 0;
+    
+    x = expandnumber(x);
+    
+    int prev;
+    int counter = 0;
+    while (prev != x) {
+        prev = x;
+         x = getsinglestep(x);
+        counter++;
+    }
+    
+    return counter - 1;
 }
 
 /*
